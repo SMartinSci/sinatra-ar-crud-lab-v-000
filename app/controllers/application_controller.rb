ccf1cf46c3708c9f207f60e8332452ad072a722e
Sherry Martin <sm674@cornell.edu>
2018-11-04 20:57:45 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,12 +27,12 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do
-    @post = Post.find(params[:id])
+    @post = Post.find_by_id(params[:id])
     erb :edit
   end
 
